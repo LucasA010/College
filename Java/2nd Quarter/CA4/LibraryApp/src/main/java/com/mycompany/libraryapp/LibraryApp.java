b71f475e93edc1736436a3f59adbd84dea0c42e0
LucasA010 <sba24010@student.cct.ie>
2025-04-08 02:16:02 +0100

    update

---
+++ Java/2nd Quarter/CA4/LibraryApp/src/main/java/com/mycompany/libraryapp/LibraryApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.libraryapp;
+
+/**
+ *
+ * @author lucru
+ */
+public class LibraryApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
