bbf1801c898fcf6ec3b79c51a0182c72a32a3c2c
LucasA010 <sba24010@student.cct.ie>
2025-04-23 19:38:22 +0100

    Fullstack 3rd quarter update

---
+++ Fullstack/3rd Quarter/blog Exercise/views/app.js
@@ -0,0 +1,74 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const PORT = 3001;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view-engine", "ejs");
+
+let blogPosts = [];
+let numPostsPerPage = 5;
+
+function makeDummyPosts(numPosts, blogPosts) {
+    for (let i=0; i<numPosts; i++) {
+        blogPosts.push({
+            author: "Author: " + i,
+            title: "test" + i,
+            content: "Text",
+            datetime: new Date().toLocaleString()
+        })
+    }
+}
+makeDummyPosts(98, blogPosts);
+
+
+function getDisplayPosts(numPostsPerPage, pagenum, blogPosts) {
+    /*
+    pagenum = 1
+    posts 0-4
+
+    pagenum = 2
+    posts 5-9
+
+    pagenum = 10
+    posts (10-1)*5 - 10*5-1
+    45-49
+    */
+   let start = (pagenum-1)*numPostsPerPage;
+   let end = pagenum*numPostsPerPage;
+   return blogPosts.slice().reverse().slice(start, end);
+}
+
+app.get("/", (req, res) => {
+    // res.sendFile("index.html", {root: __dirname});
+    let pagenum;
+    if (!req.query.pagenum) pagenum = 1;
+    else pagenum = req.query.pagenum;
+    console.log(req.query.pagenum);
+    res.render("index.ejs", { blogPosts: getDisplayPosts(numPostsPerPage, pagenum, blogPosts) });
+});
+
+// app.get('/page', (req, res) => {
+//     res.render("index.ejs");
+// });
+
+app.post('/new-blog-post', (req, res) => {
+    console.log(req.body.author);
+    console.log(req.body.title);
+    console.log(req.body.content);
+    console.log(new Date().toLocaleString());
+    blogPosts.push({
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content,
+        datetime: new Date().toLocaleString()
+    })
+    res.redirect("/");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+    
+})
