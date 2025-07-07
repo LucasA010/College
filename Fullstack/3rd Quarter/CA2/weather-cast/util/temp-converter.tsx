2716a5a051e90fbde71f1d901868101082a4e47f
Lucrujo <sba24010@student.cct.ie>
2025-07-07 13:35:22 +0100

    up

--- Fullstack/3rd Quarter/CA2/weather-cast/util/temp-converter.tsx
+++ Fullstack/3rd Quarter/CA2/weather-cast/util/temp-converter.tsx
@@ -1,5 +1,10 @@
 export const assignTemperature = (temp: number, unit: `C` | `F`): string => {
     const converted = unit === "F" ? (temp *9) /5 +32 : temp;
 
-    return `${converted}° ${unit}`
+
+    if (unit === "C") {
+    return `${Math.round(converted)}°${unit}`; 
+    } else {
+        return `${converted.toFixed(1)}°${unit}`;  
+    }
 }
